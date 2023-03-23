2670fb12cbb3b66a0cc6686a127047a89a04bc6f
fdariia <fdariya3005@gmail.com>
2023-03-24 00:09:31 +0200

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { phoneBookReducer } from './phoneBookSlice';
+
+const persistConfig = {
+  key: 'phonebook',
+  storage,
+};
+
+const rootReducers = combineReducers({ phonebook: phoneBookReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistedStore = persistStore(store);
