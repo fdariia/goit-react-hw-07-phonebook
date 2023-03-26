ff8033f2d5cd99dc7d7a32d56089d79fbf1b8a7c
fdariia <fdariya3005@gmail.com>
2023-03-27 01:39:33 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,4 @@
 export const getContacts = state => state.contacts.items;
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
-
-export const getFilteredContacts = state =>
-  state.filter(contact =>
-    contact.name.toLowerCase().includes(state.filter.toLowerCase())
-  );
+export const getFilter = state => state.filter;
